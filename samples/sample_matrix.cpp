4caf0f46eeec193b1cdbb40135496234a4860164
brimst0ne <artemchelebaev@gmail.com>
2020-10-25 20:43:11 +0300

    Second lab

---
+++ samples/sample_matrix.cpp
@@ -0,0 +1,30 @@
+// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+//
+// sample_matrix.cpp - Copyright (c) Гергель В.П. 07.05.2001
+//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (20.04.2015)
+//
+// Тестирование верхнетреугольной матрицы
+
+#include <iostream>
+#include "utmatrix.h"
+#include <iomanip>
+//---------------------------------------------------------------------------
+
+int main()
+{
+  TMatrix<int> a(5), b(5), c(5);
+  setlocale(LC_ALL, "Russian");
+  cout << "Тестирование программ поддержки представления треугольных матриц" << endl;
+  for (int i = 0; i < 5; i++)
+    for (int j = i; j < 5; j++ )
+    {
+      a[i][j] =  i * 10 + j;
+      b[i][j] = (i * 10 + j) * 100;
+    }
+  c = a + b;
+  cout << "Matrix a = " << endl << a << endl;
+  cout << "Matrix b = " << endl << b << endl;
+  cout << "Matrix c = a + b" << endl << c << endl;
+  return 0;
+}
+//---------------------------------------------------------------------------
